f1014eecfc8ad5b5425b180346191f3ee521bdac
Roman <romchikus1987@gmail.com>
2023-06-20 16:10:00 +0300

    part_5d

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -21,6 +21,7 @@ const LoginForm = ({ handleLogin }) => {
         <div>
           username
           <input
+            id="username"
             type="text"
             value={username}
             onChange={({ target }) => setUsername(target.value)}
@@ -29,12 +30,13 @@ const LoginForm = ({ handleLogin }) => {
         <div>
           password
           <input
+            id="password"
             type="password"
             value={password}
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit" style={{ marginBottom: '5px' }}>
+        <button id="login-button" type="submit" style={{ marginBottom: '5px' }}>
           login
         </button>
       </form>
