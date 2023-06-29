ad81eee5e030a79f42ec284529d868bcd23c3285
Roman <romchikus1987@gmail.com>
2023-06-29 17:42:53 +0300

    updated

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,24 +1,3 @@
-/* 
-const notesReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'SET_NOTES':
-      return action.payload
-    case 'NEW_NOTE':
-      return [...state, action.payload]
-
-    case 'TOGGLE_IMPORTANCE':
-      const id = action.payload.id
-      return state.map((note) => (note.id !== id ? note : action.payload))
-    case 'DELETE_NOTE':
-      return state.filter((note) => note.id !== action.payload)
-    default:
-      return state
-  }
-}
-
-export default notesReducer
- */
-
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import noteService from '../services/notes';
 
