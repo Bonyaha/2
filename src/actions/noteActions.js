82c86b68f54310899fcbd4733c08935464d6652e
Roman <romchikus1987@gmail.com>
2023-07-19 12:05:09 +0300

    updated

--- src/actions/noteActions.js
+++ src/actions/noteActions.js
@@ -29,7 +29,7 @@ export const addNewNote = (noteObject) => {
 
 export const toggleImportance = (id) => {
   return async (dispatch, getState) => {
-    console.log('id: ', id);
+    console.log('id: ', id)
     const notes = getState().notes
     const note = notes.find((n) => n.id === id)
     const changedNote = { ...note, important: !note.important }
